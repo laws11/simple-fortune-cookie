31ca692bcdb5c3617680918bb011ec1596182269
victor <victorsubs@pm.me>
2022-08-21 16:22:42 +0000

    fixing dockerhub #7

--- ci/build-docker-backend.sh
+++ ci/build-docker-backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}simplefortune-app:latest" -t "${DockerRepo}simplefortune-app:1.0-$Tag" frontend/
+docker build -t "${DockerRepo}simple-fortune-cookie-backend:latest" -t "${DockerRepo}simple-fortune-cookie-backend:1.0-$Tag" backend/
--- ci/push-docker-backend.sh
+++ ci/push-docker-backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}micronaut-app:latest" -t "${DockerRepo}micronaut-app:1.0-$Tag" frontend/
+docker build -t "${DockerRepo}simple-fortune-cookie-backend:latest" -t "${DockerRepo}simple-fortune-cookie-backend:1.0-$Tag" backend/
