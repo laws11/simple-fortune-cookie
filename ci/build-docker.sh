ccadb826a754a2eb71dc7aecd5ab55e7f95fb81a
victor <victorsubs@pm.me>
2022-08-21 14:01:51 +0000

    fixing dockerhub #5

---
+++ ci/build-docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
+[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
+docker build -t "${DockerRepo}simplefortune-app:latest" -t "${DockerRepo}simplefortune-app:1.0-$Tag" app/
